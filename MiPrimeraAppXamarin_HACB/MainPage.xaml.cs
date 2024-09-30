3f57163f4cbaf972f0642b668955aefadf30621b
Harol Cisneros <harolcsnrs@gmail.com>
2024-09-30 12:59:49 -0700

    Agregar archivos de proyecto.

---
+++ MiPrimeraAppXamarin_HACB/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MiPrimeraAppXamarin_HACB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
